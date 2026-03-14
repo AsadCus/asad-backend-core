895e4c42d9c81ae82c9dfdf689452948966230ae
AsadCus <icbpunvr2019@gmail.com>
2026-03-14 18:00:47 +0700

    :art:

--- resources/js/components/proper-input-select.tsx
+++ resources/js/components/proper-input-select.tsx
@@ -82,7 +82,9 @@ export function ProperInputSelect({
                     data-placeholder={!selected ? '' : undefined}
                     className={cn(
                         "flex h-9 w-full items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 text-base whitespace-nowrap shadow-xs transition-[color,box-shadow] outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[placeholder]:text-muted-foreground dark:bg-input/30 dark:hover:bg-input/50 dark:aria-invalid:ring-destructive/40 [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 [&_svg:not([class*='text-'])]:text-muted-foreground",
-                        size === 'compact' ? 'h-6 px-2 py-1 text-base sm:h-7' : '',
+                        size === 'compact'
+                            ? 'h-6 px-2 py-1 text-base sm:h-7'
+                            : '',
                         className,
                     )}
                 >
@@ -130,7 +132,7 @@ export function ProperInputSelect({
                 <Command>
                     {searchable && <CommandInput placeholder="Search..." />}
                     <CommandList className="max-h-96">
-                        <CommandGroup className="max-h-80 overflow-y-auto p-1">
+                        <CommandGroup className="max-h-76 overflow-y-auto p-1">
                             {options.map((option) => {
                                 const isSelected =
                                     String(selectedValue) ===
